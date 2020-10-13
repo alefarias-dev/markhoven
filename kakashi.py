ec417d0daeb4f38373b5274eeeddb81f2daf6e50
Ale Farias <mail.alefarias@gmail.com>
2020-10-13 20:53:42 -0300

    Do some refactoring

--- kakashi.py
+++ kakashi.py
@@ -1,72 +1,80 @@
-# kakashi eh um crawler ninja copiador de letras de musicas
-# de algum autor qualquer selecionado adicionando ao arquivo
-# training_text.txt
 import re
-import unicodedata
+from unidecode import unidecode
 
-from urllib.request import urlopen
+import requests as req
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
+
+MUSIC_SOURCE = "https://www.letras.mus.br";
+AUTORES = ["chico-buarque"]
+
+
 def normaliza_string(texto):
-    texto = unicodedata.normalize('NFKD', texto)
-    return u"".join([c for c in texto if not unicodedata.combining(c)])
+    texto = unidecode(texto)
+    texto = texto.lower()
+    return texto
 
-# buscaremos letras cadastradas no site letras
-music_source = "https://www.letras.mus.br";
-# esses sao nossos autores de interesse
-autores = ['/chico-buarque']
 
-# string que mantera as musicas
+def obtem_pagina_autor(url_autor):
+    url_autor = f"{MUSIC_SOURCE}/{autor}"
+    resposta = req.get(url_autor)
+    if checa_resposta(resposta):
+        return BeautifulSoup(resposta.text, "html.parser")
+    return None
+
+
+def obtem_pagina_musica(url_musica):
+    url_musica = f"{MUSIC_SOURCE}{url_musica}"
+    resposta = req.get(url_musica)
+    if checa_resposta(resposta):
+        return BeautifulSoup(resposta.text, "html.parser")
+    return None
+
+
+def checa_resposta(resposta_requests):
+    if resposta_requests.status_code != 200:
+        return False
+    return True
+
+
+def obtem_titulo_musica(pagina_musica):
+    try:
+        titulo_musica = pagina_musica.find("div",{"class":"cnt-head_title"})
+        titulo_musica = titulo_musica.find("h1").get_text()
+    except AttributeError as e:
+        titulo_musica = "NA"
+    return titulo_musica
+
+
+def obtem_letra_musica(pagina_musica):
+    tags_musica = pagina_musica.find("div", {"class": "cnt-letra"})
+    if tags_musica:
+        return tags_musica.text
+    return None
+
+
 temporario = ""
-modo = "w"
-sobrescreve = input("Sobrescrever arquivo de treino? (S/N)")
+for autor in AUTORES:
+    print(f"\nMinerando musicas de: {autor}")
 
-for autor in autores:
-    print ("\nMinerando musicas de: "+autor)
-    # abre a pagina do artista e transforma em um
-    # objeto BeautifulSoup
-    html = urlopen(music_source+autor)
-    bs = BeautifulSoup(html, "html.parser")
+    pagina_autor = obtem_pagina_autor(autor)
+    lista_musicas = pagina_autor.find("ol", {"class":"cnt-list"}).findAll("li")
 
-    # pega a lista de todas as musicas
-    lista_musicas = bs.find("ul", {"class":"cnt-list"})
-    lista_musicas = lista_musicas.findAll("li")
-
-    # pra cada musica
-    counter = 20
     for musica in lista_musicas:
-        counter -= 1
         a_musica = musica.find("a")
+
         if "href" in a_musica.attrs:
             link = a_musica.attrs['href'] # obtem o link para musica
-            try:
-                pagina_musica = urlopen(music_source+link).read()
-                pagina_musica = BeautifulSoup(pagina_musica, "html.parser")
+            
+            print(f"{MUSIC_SOURCE}{link}")
+            pagina_musica = obtem_pagina_musica(link)
+            titulo_musica = obtem_titulo_musica(pagina_musica)
+            letra_musica = obtem_letra_musica(pagina_musica)
+            temporario += normaliza_string(letra_musica)+"\n"
+            print(f"Musica: {titulo_musica} copiada.")
 
-                try:
-                    titulo_musica = pagina_musica.find("div",{"class":"cnt-head_title"})
-                    titulo_musica = titulo_musica.find("h1").get_text()
-                except AttributeError as e:
-                    titulo_musica = "NA"
-
-                letra_musica = pagina_musica.find("article")
-                letra_musica = str(letra_musica).replace('<p>', '')
-                letra_musica = letra_musica.replace('</p>', '\n')
-                letra_musica = letra_musica.replace('<article>', '')
-                letra_musica = letra_musica.replace('</article>', '')
-                letra_musica = letra_musica.replace('<br/>', '\n')
-                letra_musica = letra_musica.replace('\'', '')
-
-                temporario += normaliza_string(str(letra_musica))+"\n"
-                print("Musica: "+titulo_musica+" copiada pelo sharingan sinfonico.")
-
-            except HTTPError as e:
-                print(music_source+link+" falhou")
-            if counter == 0: break
 
 # escreve no arquivo
-if sobrescreve.lower() == "n": modo = "a"
-arquivo = open("training_text.txt", modo)
-arquivo.write(temporario)
-arquivo.close()
+with open("training_text.txt", "w") as arquivo:
+    arquivo.write(temporario)
